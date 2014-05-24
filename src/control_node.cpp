d9b45274b24844308e06c2d1e1a4b4a1f15ec65e
Ellon Paiva <ellonpaiva@gmail.com>
2014-05-24 21:48:26 +0200

    Add obtacle avoidance and control nodes and Controls.msg
    A control signal is defined as a geometry_msgs/Twist. But since we're
    working in a plane only linear.x/y and angular.z should be set.

---
+++ src/control_node.cpp
@@ -0,0 +1,53 @@
+#include <ros/ros.h>
+#include <futrobotros/Poses.h>
+#include <futrobotros/Controls.h>
+
+// Declare the publisher of the control signals
+ros::Publisher control_pub;
+
+/** \brief Control callback
+ *
+ * This callback should receive the reference for the robots as a futrobotros::Poses
+ * message and publish the control signals the robots should apply to reach the
+ * reference
+ */
+void controlCallback(const futrobotros::Poses::ConstPtr& msg)
+{
+	/// \todo Substitute the next line by your control function.
+	ROS_INFO("Control not implemented yet!");
+
+	// Publish robot control signals
+	/// \todo Change here for values obtained from your control function
+	futrobotros::Controls control_msg;
+	for(unsigned i=0; i<3; ++i){
+		control_msg.robots[i].linear.x = 0;
+		control_msg.robots[i].linear.y = 0;
+		control_msg.robots[i].linear.z = 0; // Should be always zero since we're in a plane
+
+		control_msg.robots[i].angular.x = 0; // Should be always zero since we only have rotations in 'z'
+		control_msg.robots[i].angular.y = 0; // Should be always zero since we only have rotations in 'z'
+		control_msg.robots[i].angular.z = 0;
+	}
+
+	control_pub.publish(control_msg);
+}
+
+int main(int argc, char **argv)
+{
+	// Init ROS
+	ros::init(argc, argv, "localization");
+
+	// Init a node handler
+	ros::NodeHandle n;
+
+	// Subscribe to the topic with the localization state
+	ros::Subscriber sub = n.subscribe("control_input", 1000, controlCallback);
+
+	// Set up strategy publisher
+	control_pub = n.advertise<futrobotros::Controls>("control_output", 1000);
+
+	// Spin until the end
+	ros::spin();
+
+	return 0;
+}
---
+++ src/obstacle_avoidance_node.cpp
@@ -0,0 +1,48 @@
+#include <ros/ros.h>
+#include <sensor_msgs/Image.h>
+#include <futrobotros/Poses.h>
+
+// Declare the publisher for robot poses after corrections
+ros::Publisher obstacle_avoidance_pub;
+
+/** \brief Obstacle Avoidance callback
+ *
+ * This callback should receive futrobotros::Poses as a message and publish
+ * the references the robots should go to avoid possible obstacles.
+ */
+void obstacleAvoidanceCallback(const futrobotros::Poses::ConstPtr& msg)
+{
+	/// \todo Substitute the next line by your strategy function.
+	ROS_INFO("Obstacle avoidance not implemented yet!");
+
+	// Publish robot references
+	/// \todo Change here for values obtained from your routine to avoid obstacles
+	futrobotros::Poses new_reference_msg;
+	for(unsigned i=0; i<3; ++i){
+		new_reference_msg.robots[i].x = 0;
+		new_reference_msg.robots[i].y = 0;
+		new_reference_msg.robots[i].theta = 0;
+	}
+
+	obstacle_avoidance_pub.publish(new_reference_msg);
+}
+
+int main(int argc, char **argv)
+{
+	// Init ROS
+	ros::init(argc, argv, "localization");
+
+	// Init a node handler
+	ros::NodeHandle n;
+
+	// Subscribe to the topic with the localization state
+	ros::Subscriber sub = n.subscribe("obstacle_avoidance_input", 1000, obstacleAvoidanceCallback);
+
+	// Set up obstacle avoidance publisher
+	obstacle_avoidance_pub = n.advertise<futrobotros::Poses>("obstacle_avoidance_output", 1000);
+
+	// Spin until the end
+	ros::spin();
+
+	return 0;
+}
