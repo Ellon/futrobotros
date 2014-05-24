18ee82035712edadee9fc0165cd2542814ca08fa
Ellon Paiva <ellonpaiva@gmail.com>
2014-05-24 21:06:54 +0200

    Change localization to publish only one topic.
    Publish multiple topics would complicate things now. Let's keep it
    simple and close to current futrobot code. Later we can try to setup a
    sistem with robot poses being published into separated topics.

--- src/localization_node.cpp
+++ src/localization_node.cpp
@@ -1,15 +1,9 @@
 #include <ros/ros.h>
 #include <sensor_msgs/Image.h>
-#include <geometry_msgs/Pose2D.h>
-#include <geometry_msgs/Point.h>
-#include <futrobotros/Poses.h>
+#include <futrobotros/State.h>
 
-// Declare individual robot poses publishers
-ros::Publisher robot_pose_pub[3];
-// Declare publisher of all robot poses at the same time
-ros::Publisher robot_poses_pub;
-// Declare publisher for the ball
-ros::Publisher ball_pos_pub;
+// Declare publisher for localization result
+ros::Publisher localization_pub;
 
 /** \brief Localization callback
  *
@@ -21,23 +15,19 @@ void localizationCallback(const sensor_msgs::Image::ConstPtr& msg)
 	/// \todo Substitute the next line by your image processing functions.
 	ROS_INFO("Image processing not implemented yet!");
 
-	// Publish robot poses
+	// Publish localization result as a state message
 	/// \todo Change here for values obtained from your image processing
-	geometry_msgs::Pose2D pose2d_msg;
+	futrobotros::State state_msg;
 	for(unsigned i=0; i<3; ++i){
-		pose2d_msg.x = 0;
-		pose2d_msg.y = 0;
-		pose2d_msg.theta = 0;
-		robot_pose_pub[i].publish(pose2d_msg);
+		state_msg.robots[i].x = 0;
+		state_msg.robots[i].y = 0;
+		state_msg.robots[i].theta = 0;
 	}
+	state_msg.ball.x = 0;
+	state_msg.ball.y = 0;
+	state_msg.ball.z = 0;
 
-	// Publish ball position
-	/// \todo Change here for values obtained from your image processing
-	geometry_msgs::Point point_msg;
-	point_msg.x = 0;
-	point_msg.y = 0;
-	point_msg.z = 0;
-	ball_pos_pub.publish(point_msg);
+	localization_pub.publish(state_msg);
 }
 
 int main(int argc, char **argv)
@@ -51,17 +41,8 @@ int main(int argc, char **argv)
 	// Subscribe to the topic with the acquired images
 	ros::Subscriber sub = n.subscribe("image_input", 1000, localizationCallback);
 
-	// Set up individual robot pose publishers
-	for(unsigned i=0; i<3; ++i){
-		std::ostringstream oss; oss << "robot/" << i << "/pose";
-		robot_pose_pub[i] = n.advertise<geometry_msgs::Pose2D>(oss.str(), 1000);
-	}
-
-	// Set up publisher for all robot poses
-	robot_poses_pub = n.advertise<futrobotros::Poses>("poses", 1000);
-
 	// Set up ball position publisher
-	ball_pos_pub = n.advertise<geometry_msgs::Point>("ball/position", 1000);
+	localization_pub = n.advertise<futrobotros::State>("state", 1000);
 
 	// Spin until the end
 	ros::spin();
