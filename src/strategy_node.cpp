8d40c0e16d36b970183c56c8f562bf9e05d6abc4
Ellon Mendes <ellon.mendes@laas.fr>
2017-01-13 02:23:55 +0100

    Use message_filters to synchronize callback with all input msgs

--- src/strategy_node.cpp
+++ src/strategy_node.cpp
@@ -1,6 +1,9 @@
 #include <ros/ros.h>
+#include <message_filters/subscriber.h>
+#include <message_filters/time_synchronizer.h>
 #include <futrobotros/State.h>
 #include <futrobotros/TeamPose.h>
+#include <geometry_msgs/PointStamped.h>
 
 // Declare the robot poses publishers
 ros::Publisher strategy_pub;
@@ -10,7 +13,10 @@ ros::Publisher strategy_pub;
  * This callback should receive futrobotros::State as a message and publish
  * the references the robots should go following the defined strategy
  */
-void strategyCallback(const futrobotros::TeamPose::ConstPtr& msg)
+void strategyCallback(
+	const futrobotros::TeamPose::ConstPtr& team_msg,
+	const futrobotros::TeamPose::ConstPtr& opponent_msg,
+	const geometry_msgs::PointStamped::ConstPtr& ball_msg)
 {
 	/// \todo Substitute the next line by your strategy function.
 	ROS_INFO("Strategy not implemented yet!");
@@ -35,9 +41,14 @@ int main(int argc, char **argv)
 	// Init a node handler
 	ros::NodeHandle n;
 
-	// Subscribe to the topic with the localization state
-	ros::Subscriber sub = n.subscribe("strategy_input", 1000, strategyCallback);
-
+	// Declare subscribers using message_filters for synchronization
+	message_filters::Subscriber<futrobotros::TeamPose> team_pose_sub(n, "team_poses", 1000);
+	message_filters::Subscriber<futrobotros::TeamPose> opponent_pose_sub(n, "opponent_poses", 1000);
+	message_filters::Subscriber<geometry_msgs::PointStamped> ball_position_sub(n, "ball_position", 1000);
+	// Synchronize and register the callback
+	message_filters::TimeSynchronizer<futrobotros::TeamPose, futrobotros::TeamPose, geometry_msgs::PointStamped> sync(team_pose_sub, opponent_pose_sub, ball_position_sub, 10);
+	sync.registerCallback(boost::bind(&strategyCallback, _1, _2, _3));
+ 
 	// Set up strategy publisher
 	strategy_pub = n.advertise<futrobotros::TeamPose>("strategy_output", 1000);
 
