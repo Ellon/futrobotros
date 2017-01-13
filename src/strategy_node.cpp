28b64a70c192c01021eeafd1d401d94e27ae9ade
Ellon Mendes <ellon.mendes@laas.fr>
2017-01-13 18:39:08 +0100

    Make strategy publish bypassControl and blocked messages

--- src/strategy_node.cpp
+++ src/strategy_node.cpp
@@ -4,13 +4,19 @@
 #include <geometry_msgs/PointStamped.h>
 
 #include <futrobotros/BallVelocity.h>
+#include <futrobotros/TeamBlocked.h>
+#include <futrobotros/TeamBypassControl.h>
 #include <futrobotros/TeamPose.h>
 
 #include "strategy.h"
 
 // Declare the robot poses publishers
 ros::Publisher references_pub;
+ros::Publisher bypass_control_pub;
+ros::Publisher blocked_pub;
+
 Strategy * p_strategy;
+
 /** \brief Strategy callback
  *
  * This callback should receive futrobotros::State as a message and publish
@@ -58,9 +64,35 @@ void strategyCallback(
 		reference_msg.robot_pose[i].y = ref.me[i].y();
 		reference_msg.robot_pose[i].theta = ref.me[i].theta();
 	}
-
 	// Publish team reference msg
 	references_pub.publish(reference_msg);
+
+	// Recover bypassControl state, convert to message and publish
+	bool bypassControl[3];
+	PWM_ROBOTS pwm;
+	p_strategy->get_bypass_control(bypassControl, pwm);
+	futrobotros::TeamBypassControl bypass_control_msg;
+	bypass_control_msg.header.stamp = team_msg->header.stamp;
+	// bypass_control_msg.header.seq = team_msg->header.seq;
+	bypass_control_msg.header.frame_id = team_msg->header.frame_id; // Should be the same?
+	for(unsigned i=0; i<3; ++i){
+		bypass_control_msg.bypass[i] = bypassControl[i];
+		bypass_control_msg.robot_pwm[i].left = pwm.me[i].left;
+		bypass_control_msg.robot_pwm[i].right = pwm.me[i].right;
+	}
+	bypass_control_pub.publish(bypass_control_msg);
+
+	// Recover blocked state, convert to message and publish
+	bool bloqueado[3];
+	p_strategy->get_bloqueado(bloqueado);
+	futrobotros::TeamBlocked blocked_msg;
+	blocked_msg.header.stamp = team_msg->header.stamp;
+	// blocked_msg.header.seq = team_msg->header.seq;
+	blocked_msg.header.frame_id = team_msg->header.frame_id; // Should be the same?
+	for(unsigned i=0; i<3; ++i){
+		blocked_msg.blocked[i] = bloqueado[i];
+	}
+	blocked_pub.publish(blocked_msg);
 }
 
 int main(int argc, char **argv)
@@ -91,6 +123,8 @@ int main(int argc, char **argv)
 
 	// Set up strategy publisher
 	references_pub = n.advertise<futrobotros::TeamPose>("team_references", 10);
+	bypass_control_pub = n.advertise<futrobotros::TeamBypassControl>("team_bypass_control", 10);
+	blocked_pub = n.advertise<futrobotros::TeamBlocked>("team_blocked", 10);
 
 	// Spin until the end
 	ros::spin();
